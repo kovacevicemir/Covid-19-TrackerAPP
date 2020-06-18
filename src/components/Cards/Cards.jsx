8166495600b05b030982c3e32ac0d45de71958f9
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-18 18:05:24 +1000

    implementation of Material UI grid n cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import styles from './Cards.module.css'
+import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core'
 
-const Cards = () => {
+const Cards = (props) => {
+    console.log(props)
     return (
-        <div>
-            <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing = {3} justify = "center">
+                <Grid item component = {Card}>
+                    <CardContent>
+                        <Typography color = "textSecondary" gutterBottom>
+                            Infected
+                        </Typography>
+                        <Typography variant="h5" gutterBottom>
+                            REAL DATA
+                        </Typography>
+                        <Typography color = "textSecondary" gutterBottom>
+                            REAL DATE
+                        </Typography>
+                        <Typography variant="body2" gutterBottom>
+                            Number of active cases of COVID-19
+                        </Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
         </div>
     )
 }
