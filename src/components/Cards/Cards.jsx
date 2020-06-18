2a27376b23f307786a5d827f4daf5948b4b6c876
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-18 18:29:21 +1000

    use of classnames and linking Cards.module.css with grid

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Cards.module.css'
 import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core'
 import CountUp from 'react-countup'
+import cx from 'classnames'
 
 const Cards = ({data: {confirmed, recovered, lastUpdate, deaths}}) => {
     if(!confirmed){
@@ -11,7 +12,7 @@ const Cards = ({data: {confirmed, recovered, lastUpdate, deaths}}) => {
     return (
         <div className={styles.container}>
             <Grid container spacing = {3} justify = "center">
-                <Grid item component = {Card}>
+                <Grid item component = {Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>
                             Infected
@@ -32,7 +33,7 @@ const Cards = ({data: {confirmed, recovered, lastUpdate, deaths}}) => {
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component = {Card}>
+                <Grid item component = {Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>
                             Recovered
@@ -53,7 +54,7 @@ const Cards = ({data: {confirmed, recovered, lastUpdate, deaths}}) => {
                         </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component = {Card}>
+                <Grid item component = {Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color = "textSecondary" gutterBottom>
                             Deaths
